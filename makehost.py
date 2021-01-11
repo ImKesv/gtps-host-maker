a0207179e7084faeb271c41c7539548b521d2181
ImKesv <77145200+ImKesv@users.noreply.github.com>
2021-01-11 20:30:02 +0700

    Update makehost.py

--- makehost.py
+++ makehost.py
@@ -5,7 +5,7 @@ def main():
     ip = input("Your Vps Ip: ")
     with open("host.txt", "w") as f:
         f.write(f"{ip} growtopia1.com\n")
-        f.write(f"{ip} growtopia2.com\n")
+        f.write(f"{ip} growtopia2.com")
         print("done")
 
 if __name__ == "__main__":
